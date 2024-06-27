33929858a8750ee0eaa0e1e3fb4cad1005bc4344
Guillem <guillemus@proton.me>
2024-06-27 11:29:47 -0400

    fix linter

--- destination/destination.go
+++ destination/destination.go
@@ -274,7 +274,7 @@ func (m *multiStreamWriter) Write(ctx context.Context, records []sdk.Record) (in
 
 	if m.streamProvisioner != nil {
 		if err := m.streamProvisioner.ensureStreamsExist(ctx, m.destination, batches); err != nil {
-		        return 0, fmt.Errorf("failed to ensure streams exist: %w", err)
+			return 0, fmt.Errorf("failed to ensure streams exist: %w", err)
 		}
 	}
 
